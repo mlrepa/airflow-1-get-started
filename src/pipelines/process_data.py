efe2fec4b4ae3150ed550508a8607871a622a214
Mikhail <mnrozhkov@gmail.com>
2023-06-27 11:28:48 +0200

    Fix incorrect MAPE value: Filter out data where target column value equals to zero

--- src/pipelines/process_data.py
+++ src/pipelines/process_data.py
@@ -25,6 +25,15 @@ def process() -> None:
         data.duration_min = data.duration_min.apply(
             lambda td: float(td.total_seconds() / 60)
         )
+        
+        # Drop unused columns
+        data = data.drop(["store_and_fwd_flag"], axis=1)
+
+        # Fill missing values with the median for numeric columns only
+        numeric_columns = data.select_dtypes(include='number').columns
+        medians = data[numeric_columns].median()
+        data = data.fillna(medians).fillna(0)
+        data = data[data['duration_min'] != 0]
 
         print("Save data")
         path_destination = f'{FEATURES_DIR}/{file}'
