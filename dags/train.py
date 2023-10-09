0532b235d18e508d7f4de25d8c5896578f5f9664
AlexanderKolosov <alexckolosov@gmail.com>
2023-10-09 20:54:31 +0900

    Add train DAG

---
+++ dags/train.py
@@ -0,0 +1,33 @@
+import os
+
+import pendulum
+from airflow.operators.bash import BashOperator
+
+from airflow import DAG
+from config import BATCH_INTERVAL, END_DATE_TIME, START_DATE_TIME
+
+dag = DAG(
+    dag_id="train",
+    start_date=pendulum.parse(START_DATE_TIME),
+    end_date=pendulum.parse(END_DATE_TIME),
+    schedule_interval="@weekly",
+    max_active_runs=1,
+)
+
+with dag:
+
+    PROJECT_DIR = os.environ["PROJECT_DIR"]
+    TS = "{{ ts }}"  # The DAG run’s logical date
+
+    train = BashOperator(
+        task_id="train",
+        bash_command=f"""
+
+            cd $PROJECT_DIR && echo $PWD && \
+            export PYTHONPATH=. && echo $PYTHONPATH && \
+
+            python src/pipelines/train.py
+        """
+    )
+
+    train
